e1976373a60ad1c9d6dfb0b9936075eed82091fc
Alexander Shioshvili <32383409+ashioshvili@users.noreply.github.com>
2017-10-26 17:21:33 +0400

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ bot = Bot(PAGE_ACCESS_TOKEN)
 @app.route('/', methods=['GET'])
 def verify():
 	#Webhook verification
-	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challange"):
+	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
 		if not request.args.get("hub.verify_token") == "hello":
 			return "Verification token mismatch", 403
-		return request.args["hub.challange"], 200
+		return request.args["hub.challenge"], 200
 	return "Hello world", 200
 	
 @app.route('/', methods=['POST'])
